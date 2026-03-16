9df2f1e9d1336c65d5015ad949687aba8bc53326
Rosa Cabrera <mrcabreracorrea@gmail.com>
2026-03-16 15:13:12 -0300

    feat: implement main layout wrapper with dynamic title

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
+import tseslint from 'typescript-eslint';
 
 export default [
   ...eslintPluginAstro.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
