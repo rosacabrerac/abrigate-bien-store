060b2204d832e9fa464035102188c8d60642660e
Rosa Cabrera <mrcabreracorrea@gmail.com>
2026-03-24 14:55:33 -0300

    feat: implement cart store and item addition logic

---
+++ src/store/cartStore.ts
@@ -0,0 +1,31 @@
+import { map } from "nanostores";
+
+export type CartItem = {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+  size: string;
+  color: string;
+  quantity: number;
+};
+
+export const cartItems = map<Record<string, CartItem>>({});
+
+export function addItemsToCart(item: CartItem) {
+  const cartItemId = `${item.id}-${item.size}-${item.color}`;
+
+  const existingItem = cartItems.get()[cartItemId];
+
+  if (existingItem) {
+    cartItems.setKey(cartItemId, {
+      ...existingItem,
+      quantity: existingItem.quantity + 1,
+    });
+  } else {
+    cartItems.setKey(cartItemId, {
+      ...item,
+      quantity: 1,
+    });
+  }
+};
